42c0605a9ed8a4fcdaddbab82453442c65394e71
P0LoYT <56827504+P0LoYT@users.noreply.github.com>
2022-04-06 14:32:41 +0300

    Add files via upload

---
+++ src/script.js
@@ -0,0 +1,22 @@
+mapboxgl.accessToken = 'pk.eyJ1IjoicDBsbyIsImEiOiJjbDFtYmhvOXMwMTFvM29wZ2R4ODBlbXhzIn0.ZPmr2huPPp-xxVVfA_6u5Q';
+
+
+var map = new mapboxgl.Map({
+
+container: 'map',
+style: 'mapbox://styles/mapbox/streets-v11',
+center: [30, 50], 
+zoom: 9 
+
+})
+
+
+map.addControl(new mapboxgl.NavigationControl());
+
+
+var directions = new MapboxDirections({
+    accessToken: mapboxgl.accessToken,
+  });
+
+
+map.addControl(directions, 'top-left');
